1188b800dd755898ea9225a0c114ce7ef3dbbe78
ossan05 <oskar.kinell@bahnhof.se>
2023-03-13 14:25:04 +0100

    graph completed

--- julia/Q5.jl
+++ julia/Q5.jl
@@ -6,6 +6,8 @@ data = Matrix{Float64}(X)
 
 M = fit(MDS, data; maxoutdim=2, distances=true)
 Y = predict(M)
+println(Y[1, 430])
+println(Y[2, 430])
 
 scatter(Y[1,:], Y[2,:], marker = :circle, linewidth=0)
-scatter!(Y)
+scatter!((Y[1,430], Y[2,430]), marker = :circle, linewidth=0, color = :orange)
